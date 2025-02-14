4a9065e1af4b3859748a2b0942b1c6301ab6ffde
Anjali Haryani <HaryaniAnjali@users.noreply.huggingface.co>
2025-02-14 02:02:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,4 @@
 import gradio as gr
-from transformers import pipeline   
-
-# Load your model from Hugging Face
-model_name = "HaryaniAnjali/Llama_3.2_Trained_Emotion"
-classifier = pipeline("Emotion-Classification", model=model_name)
-
-def classify_text(text):
-    return classifier(text)
-
-# Create a Gradio interface
-interface = gr.Interface(fn=classify_text, inputs="text", outputs="json")
-interface.launch()
-
-
-import gradio as gr
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
