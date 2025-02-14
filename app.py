fe9264b48f66792ffb3f59c42d575bc12791f11c
Anjali Haryani <HaryaniAnjali@users.noreply.huggingface.co>
2025-02-14 01:23:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import gradio as gr
 from transformers import pipeline
 
 # Load your model from Hugging Face
-model_name = "your-username/your-model-name"
-classifier = pipeline("text-classification", model=model_name)
+model_name = "HaryaniAnjali/Llama_3.2_Trained_Emotion"
+classifier = pipeline("Emotion-Classification", model=model_name)
 
 def classify_text(text):
     return classifier(text)
